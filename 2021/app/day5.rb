8debd99c3f69579da4bd996d197609e1cc82162e
Junze He <junzenology@gmail.com>
2021-02-04 22:59:36 -0600

    Day 5

---
+++ 2021/app/day5.rb
@@ -0,0 +1,82 @@
+module Day5
+  module_function
+
+  NUM_ROWS = 128
+  NUM_COLS = 8
+  def seat_row(row_desc)
+    rows = (0..NUM_ROWS-1).to_a
+
+    row_desc.split("").each do |letter|
+      midpoint = (rows.length/2.0) - 1
+      case letter
+      when 'F'
+        rows = rows[0..midpoint]
+      when 'B'
+        rows = rows[midpoint+1..]
+      end
+    end
+
+    rows.first
+  end
+
+  def seat_column(column_desc)
+    columns = (0..NUM_COLS-1).to_a
+
+    column_desc.split("").each do |letter|
+      midpoint = (columns.length/2.0) - 1
+      case letter
+      when 'L'
+        columns = columns[0..midpoint]
+      when 'R'
+        columns = columns[midpoint+1..]
+      end
+    end
+
+    columns.first
+  end
+
+  def seat_id(seat_location)
+    row_desc = seat_location[0..7]
+    column_desc = seat_location[7..]
+
+    (seat_row(row_desc) * 8) + seat_column(column_desc)
+  end
+
+  def part1
+    ids = []
+    File.open('./app/day5_input').each_line do |line|
+      ids <<  seat_id(line)
+    end
+
+    puts "Part1 Max: #{ids.max}"
+  end
+
+  def part2
+    ids = []
+    File.open('./app/day5_input').each_line do |line|
+      ids <<  seat_id(line)
+    end
+    ids = ids.sort
+
+    my_seat_id = nil
+    ids.each_with_index do |id, index|
+      next if index == 0 || index == ids.length - 1
+
+      prev_id = ids[index - 1]
+      next_id = ids[index + 1]
+
+      if next_id != (id + 1)
+        my_seat_id = id + 1
+      end
+    end
+
+    puts "Part2 #{my_seat_id}"
+  end
+
+  def main
+    part1
+    part2
+  end
+end
+
+Day5.main if __FILE__ == $PROGRAM_NAME
---
+++ 2021/spec/day5_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Day5 do
+  describe '#part1' do
+    describe "seat_row" do
+      it "returns the seat row given the first seven characters of a seat location" do
+        expect(Day5.seat_row("FBFBBFF")).to eq(44)
+        expect(Day5.seat_row("BFFFBBF")).to eq(70)
+        expect(Day5.seat_row("FFFBBBF")).to eq(14)
+        expect(Day5.seat_row("BBFFBBF")).to eq(102)
+      end
+    end
+
+    describe "seat_column" do
+      it "returns the seat column given the last 3 characters of a seat location" do
+        expect(Day5.seat_column("RLR")).to eq(5)
+        expect(Day5.seat_column("RRR")).to eq(7)
+        expect(Day5.seat_column("RRR")).to eq(7)
+        expect(Day5.seat_column("RLL")).to eq(4)
+      end
+    end
+
+    describe "seat_id" do
+      it "calculates the seat_id given a seat_location" do
+        expect(Day5.seat_id("FBFBBFFRLR")).to eq(357)
+        expect(Day5.seat_id("BFFFBBFRRR")).to eq(567)
+        expect(Day5.seat_id("FFFBBBFRRR")).to eq(119)
+        expect(Day5.seat_id("BBFFBBFRLL")).to eq(820)
+      end
+    end
+  end
+
+  describe '#part2' do
+  end
+end

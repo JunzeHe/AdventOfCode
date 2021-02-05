ab5caffc8fe292c82fdaa3bd260545bbbd2035d6
Junze He <junzenology@gmail.com>
2021-02-04 21:49:55 -0600

    Template files

---
+++ 2021/app/day_x_template.rb
@@ -0,0 +1,21 @@
+module DayX
+  module_function
+
+  def parse(raw_input)
+  end
+
+  def part1
+    puts "Part1 #{}"
+  end
+
+  def part2
+    puts "Part2 #{}}"
+  end
+
+  def main
+    part1
+    part2
+  end
+end
+
+DayX.main if __FILE__ == $PROGRAM_NAME
---
+++ 2021/spec/day_x_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe DayX do
+  describe '#part1' do
+  end
+
+  describe '#part2' do
+  end
+end
